f3512d965c022daa722a49e1b8901290136bc9ef
Thomas Noone <tnoone@lcs.on.ca>
2024-12-17 09:45:41 -0500

    Initial Commit

---
+++ HobbyCards/HobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyCardsApp.swift
+//  HobbyCards
+//
+//  Created by Thomas Noone on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct HobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
