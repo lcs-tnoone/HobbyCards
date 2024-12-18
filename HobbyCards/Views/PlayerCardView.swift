b27fd66e4ef7a6861243dd453b688bf80772bc04
Thomas Noone <tnoone@lcs.on.ca>
2024-12-18 12:50:03 -0500

    changed from Vstack to List to make sizing easier

--- HobbyCards/Views/PlayerCardView.swift
+++ HobbyCards/Views/PlayerCardView.swift
@@ -12,85 +12,90 @@ struct PlayerCardView: View {
     let chosenPlayer: Player
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Image(chosenPlayer.playerImage)
-                .resizable()
-                .scaledToFit()
-            //                    .frame(width: 350, height: 233)
-                .background(Color.white)
-                .border(Color.black, width: 3)
-                .clipped()
-                .padding(40)
-                .overlay(alignment: .topLeading) {
-                    ZStack {
-                        XShape()
-                            .fill(Color.red)
-                        XShape()
-                            .stroke(Color.black, lineWidth: 5)
-                        Image(systemName: "globe")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.black)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(chosenPlayer.playerImage)
+                    .resizable()
+                    .scaledToFit()
+                //                    .frame(width: 350, height: 233)
+                    .background(Color.white)
+                    .border(Color.black, width: 3)
+                    .clipped()
+                    .padding(40)
+                    .overlay(alignment: .topLeading) {
+                        ZStack {
+                            XShape()
+                                .fill(Color.red)
+                            XShape()
+                                .stroke(Color.black, lineWidth: 5)
+                            Image(systemName: "globe")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.black)
+                        }
+                        .frame(width: 100, height: 100)
+                        //                        .offset(x: -25, y: -25)
                     }
-                    .frame(width: 100, height: 100)
-                    //                        .offset(x: -25, y: -25)
-                }
-            Text(chosenPlayer.name)
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-                .padding(.vertical, 10)
-                .frame(maxWidth: .infinity, alignment: .center)
-            PodiumShape()
-                .fill(
-                    LinearGradient(
-                        gradient: Gradient(colors: [.red, .black]),
-                        startPoint: .top,
-                        endPoint: .bottom
+                    .border(.red)
+                Text(chosenPlayer.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding(.bottom, 10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .offset(y: -30)
+                PodiumShape()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.red, .black]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
                     )
-                )
-//                .scaleEffect(x: 1, y: 0.4)
-                .overlay {
-                    ZStack {
-                        // Top XShape
-                        VStack {
-                            XShape()
-                                .fill(Color.yellow)
-                                .frame(width: 80, height: 80)
-                            Text("1")
-                                .font(.caption)
-                                .foregroundColor(.black)
+                    .frame(height: 200)
+    //                .scaleEffect(x: 1, y: 0.4)
+                    .overlay {
+                        ZStack {
+                            // Top XShape
+                            VStack {
+                                XShape()
+                                    .fill(Color.yellow)
+                                    .frame(width: 80, height: 80)
+                                Text("\(chosenPlayer.goldMedals)")
+                                    .font(.caption)
+                                    .foregroundColor(.yellow)
+                            }
+                            .offset(y: -20)
+                            
+                            // Bottom-left XShape
+                            VStack {
+                                XShape()
+                                    .fill(Color.gray)
+                                    .frame(width: 60, height: 60)
+                                Text("\(chosenPlayer.silverMedals)")
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                            }
+                            .offset(x: -50, y: 40)
+                            
+                            // Bottom-right XShape
+                            VStack {
+                                XShape()
+                                    .fill(Color.brown)
+                                    .frame(width: 60, height: 60)
+                                Text("\(chosenPlayer.bronzeMedals)")
+                                    .font(.caption)
+                                    .foregroundColor(.brown)
+                            }
+                            .offset(x: 50, y: 40)
                         }
-                        .offset(y: -20)
-                        
-                        // Bottom-left XShape
-                        VStack {
-                            XShape()
-                                .fill(Color.gray)
-                                .frame(width: 60, height: 60)
-                            Text("1")
-                                .font(.caption)
-                                .foregroundColor(.black)
-                        }
-                        .offset(x: -50, y: 40)
-                        
-                        // Bottom-right XShape
-                        VStack {
-                            XShape()
-                                .fill(Color.brown)
-                                .frame(width: 60, height: 60)
-                            Text("1")
-                                .font(.caption)
-                                .foregroundColor(.black)
-                        }
-                        .offset(x: 50, y: 40)
                     }
-                }
-            
-                .padding(5)
-            
-            Spacer()
+                Text(chosenPlayer.description)
+                    .padding(5)
+                
+                Spacer()
+            }
         }
     }
 }
