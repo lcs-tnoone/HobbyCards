a08150a0d0df2193f4ec7546129d2c41a6f9b295
Thomas Noone <tnoone@lcs.on.ca>
2024-12-18 11:31:07 -0500

    Finished X games Logo shape, Added image for Dusty

--- HobbyCards/Shape/XShape.swift
+++ HobbyCards/Shape/XShape.swift
@@ -1,11 +1,3 @@
-//
-//  X Shape.swift
-//  HobbyCards
-//
-//  Created by Thomas Noone on 2024-12-17.
-//
-
-
 import SwiftUI
 
 struct XShape: Shape {
@@ -23,11 +15,29 @@ struct XShape: Shape {
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY * 0.25))
         path.addLine(to: CGPoint(x: rect.maxX * 0.75, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY * 0.25))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.25, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY * 0.25))
         
         return path
     }
 }
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            XShape()
+                .fill(Color.red) // Fill the inside of the X with red
+            XShape()
+                .stroke(Color.black, lineWidth: 20) // Add a black border to the X
+            Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50) // Adjust the size of the globe
+                .foregroundColor(.black) // Make the globe black
+        }
+        .frame(width: 200, height: 200) // Set the overall size of the shape
+    }
+}
 
 #Preview {
-    XShape()
+    ContentView()
 }
--- HobbyCards/Views/PlayerCardView.swift
+++ HobbyCards/Views/PlayerCardView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct PlayerCardView: View {
+    
+    let chosenPlayer: Player
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            ZStack{
+                Image(chosenPlayer.playerImage)
+                    .resizable()
+                    .scaledToFit()
+                ZStack {
+                    XShape()
+                        .fill(Color.red) // Fill the inside of the X with red
+                    XShape()
+                        .stroke(Color.black, lineWidth: 20) // Add a black border to the X
+                    Image(systemName: "globe")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50) // Adjust the size of the globe
+                        .foregroundColor(.black) // Make the globe black
+                }
+                .frame(width: 200, height: 200) // Set the overall size of the shape
+            
+
+            }
+        }
+    }
+    
+}
+
+
+#Preview {
+    NavigationStack{
+        PlayerCardView(chosenPlayer: DustyHenrickson)
     }
 }
 
-#Preview {
-    PlayerCardView()
-}
